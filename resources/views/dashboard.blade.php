a856e05492727dd3f0841e005917a4da774a8b78
Yura Manko <manko.yuriy@gmail.com>
2023-07-03 18:01:36 +0300

    add form for create posts

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -4,12 +4,21 @@
             {{ __('Dashboard') }}
         </h2>
     </x-slot>
-
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 text-gray-900">
-                    {{ __("You're logged in!") }}
+                    <form action="">
+                        <div class="form-group">
+                            <label for="title">Title</label>
+                            <input type="email" class="form-control" id="title" placeholder="Title for the Post">
+                        </div>
+                        <div class="form-group">
+                            <label for="description">Description</label>
+                            <textarea class="form-control" id="description" rows="5" placeholder="Enter Description for the Post"></textarea>
+                        </div>
+                        <button type="submit" class="btn btn-primary">Add Post</button>
+                    </form>
                 </div>
             </div>
         </div>
